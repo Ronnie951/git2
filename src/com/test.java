e9b7d6362721c9ebee6a1cfbaa39d4b7eec5e7ec
Ronnie <15837116943@163.com>
2018-11-27 14:48:27 +0800

    克隆后修改传递

--- src/com/test.java
+++ src/com/test.java
@@ -3,10 +3,12 @@ package com;
 public class test {
     public static void main(String[] args) {
         System.out.println("测试");
+        //添加一行代码
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
